25181799c79fba122b920b33b7e9eac53d976d9c
morcillo muriel <morcillomuriel@gmail.com>
2019-03-27 17:19:05 +0100

    exercice filter 1

--- filter/ex1.js
+++ filter/ex1.js
@@ -12,7 +12,11 @@ Sortie attendue:
  */
 
 function getPositiveNumbers(numbers) {
-}
+  const numbersPos = numbers.filter(function (number){
+        return number >= 0;
+  });
+  return numbersPos;
+};
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
